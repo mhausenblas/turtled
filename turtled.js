679e0d0942d613a50d73432ccbe8641523f01a38
Michael Hausenblas <michael.hausenblas@gmail.com>
2012-07-27 18:46:30 +0100

    init rendering

--- turtled.js
+++ turtled.js
@@ -1,4 +1,17 @@
+var graph = Viva.Graph.graph();
+var graphics = Viva.Graph.View.svgGraphics();
+var layout = Viva.Graph.Layout.forceDirected(graph, {
+	springLength: 30,
+	springCoeff: 0.0008,
+	dragCoeff: 0.009,
+	gravity: -1.2,
+	theta: 0.8
+});
+
 $(document).ready(function(){
+
+	$("#out").css('margin-left', $("#in").width() + 10);
+
 	rdfstore.create(function(store) {
 		
 		$("#vis").click(function(event){
@@ -9,6 +22,8 @@ $(document).ready(function(){
 			store.load("text/turtle", tinput, function(success, results) {
 				if(success){
 					status("Valid RDF Turtle. Loaded <strong>" + results + "</strong> triples.");
+					graph.addLink(1, 2);
+					renderGraph("out");
 				}
 				else {
 					status("<span style='color:red'>Invalid RDF Turtle :(</span>" );
@@ -35,3 +50,15 @@ function listTriples(){
 	  }
 	});
 }
+
+function renderGraph(containerID){
+	var renderer = Viva.Graph.View.renderer(graph, {
+		layout: layout,
+		graphics: graphics,
+		renderLinks: true,
+		container: document.getElementById(containerID)
+	});
+	renderer.run();
+}
+
+
